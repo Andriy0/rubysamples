7fb948cfdd59f311469d26e227bc5c2d156fd6de
Andriy0 <andriy2152@gmail.com>
2022-04-17 18:36:07 +0300

    Fix rubocop warnings in test_cargohold.rb

--- test_cargohold.rb
+++ test_cargohold.rb
@@ -1,4 +1,6 @@
-require_relative "cargohold"
+# frozen_string_literal: true
+
+require_relative 'cargohold'
 
 ch = CargoHold.new
 sc1 = Suitcase.new
@@ -7,7 +9,6 @@ sc3 = Suitcase.new
 ch.load_and_report(sc1)
 ch.load_and_report(sc2)
 ch.load_and_report(sc3)
-first_unloaded = ch.unljoad
-print "The first suitcase off the plane is..."
+first_unloaded = ch.unload
+print 'The first suitcase off the plane is...'
 puts first_unloaded.object_id
-
